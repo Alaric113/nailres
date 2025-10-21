89ea523a50ff7a5af82e387e70615eef193ac145
Alaric113 <85896029+Alaric113@users.noreply.github.com>
2025-10-21 12:19:40 +0800

    1

---
+++ src/swiper.d.ts
@@ -0,0 +1,6 @@
+// This declaration file tells TypeScript that it's okay to import these CSS files as modules.
+// It resolves the "Cannot find module" error (TS2307) during the build process (tsc).
+
+declare module 'swiper/css';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
