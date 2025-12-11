656867175b9fef14f4fbcd320fd60507f4d2f737
Alaric113 <yehweber24@gmail.com>
2025-12-11 20:21:05 +0800

    Update BookingPage.tsx

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 import ServiceSelector from '../components/booking/ServiceSelector';
