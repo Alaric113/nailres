58931ceaf296d07c0fb3b36c14026c1c1c3bc732
Alaric113 <85896029+Alaric113@users.noreply.github.com>
2026-01-05 11:18:47 +0800

    test

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -26,7 +26,7 @@ const LoadingSpinner = ({
         }
         
         .loader_ring {
-          stroke: #EFECE5; /* Secondary Light - Background Ring */
+          stroke: var(--color-secondary-light); /* Secondary Light - Background Ring */
           transition: stroke 0.3s;
         }
         
@@ -84,7 +84,7 @@ const LoadingSpinner = ({
       <svg className="loader" viewBox="0 0 128 128" width={sizePx} height={sizePx} xmlns="http://www.w3.org/2000/svg">
         <g fill="none" strokeLinecap="round" strokeWidth="16">
           <circle className="loader_ring" r="56" cx="64" cy="64"></circle>
-          <g stroke="#9F9586"> {/* Primary Color */}
+          <g stroke="var(--color-primary)"> {/* Primary Color */}
             <path className="loader_worm1" d="M120,64c0,30.928-25.072,56-56,56S8,94.928,8,64" strokeDasharray="43.98 307.87"></path>
             <g transform="translate(42,42)">
               <g className="loader_worm2" transform="translate(-42,0)">
@@ -98,7 +98,7 @@ const LoadingSpinner = ({
       {/* Loading Text */}
       {text && (
         <div className="text-center">
-          <p className="text-[#9F9586] font-medium font-serif animate-pulse tracking-wide">{text}</p>
+          <p className="text-[var(--color-primary)] font-medium font-serif animate-pulse tracking-wide">{text}</p>
         </div>
       )}
     </div>
